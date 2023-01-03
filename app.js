9a1c5a2364684e964858e9ac8a5edef559c41277
Alfonso JL <ajavierlombardi@gmail.com>
2023-01-02 23:46:13 -0300

    ajustes iconos footer

--- app.js
+++ app.js
@@ -18,5 +18,5 @@ app.get('/login', (req, res) => {
 });
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
-    console.log('Servidor iniciado en http://localhost:3030' + port);
+    console.log('Servidor iniciado en http://localhost:' + port);
 });
